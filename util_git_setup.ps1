bc3438fa96f4f65ead22c0f885128a9e69188cd7
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-10-15 16:59:16 -0700

    Update git setup script to actually clone repos

--- util_git_setup.ps1
+++ util_git_setup.ps1
@@ -5,10 +5,7 @@ Enum EXIT_CODE {
 }
 
 function PrintError($msg) {
-    PushCtx | Out-Null
-    $host.ui.RawUI.ForegroundColor = "Red"
-    Write-Host "ERROR: $msg"
-    PopCtx | Out-Null
+    Write-Error "$msg"
 }
 
 function ReadHost($prompt) {
@@ -45,14 +42,14 @@ function Quit([EXIT_CODE]$code) {
             $host.ui.RawUI.ForegroundColor = "Green"
         }
         INCOMPLETE {
-            $host.ui.RawUI.ForegroundColor = "YELLOW"
+            $host.ui.RawUI.ForegroundColor = "Yellow"
         }
         ERROR {
-            $host.ui.RawUI.ForegroundColor = "Green"
+            $host.ui.RawUI.ForegroundColor = "Red"
         }
     }
 
-    Write-Host "EXIT: $code"
+    Write-Host "`nEXIT: $code"
 
     # restore to startup state
     while (PopCtx) {
@@ -62,7 +59,7 @@ function Quit([EXIT_CODE]$code) {
 }
 
 function ValidatePath($path) {
-    if ([string]::IsNullOrEmpty($gitDir)) {
+    if ([string]::IsNullOrEmpty($path)) {
         PrintError("Invalid path")
         return $false
     }
@@ -78,7 +75,6 @@ function ValidatePath($path) {
 }
 
 function Confirm($prompt) {
-    $response = $false
     while (1) {
         $response = ReadHost("$prompt [y/n]")
 
@@ -98,32 +94,69 @@ function Confirm($prompt) {
 function Main() {
     $gitDir = $null
     
+    # ask user where to put the git directory
     do {
         $gitDir = ReadHost("Select git directory")
     } while (-not(ValidatePath($gitDir)))
-
-    if (-not(Confirm("Make `"$gitDir`" your primary git directory?"))) {
+    
+    $gitDirUpper = $gitDir.toUpper()
+    if (-not(Confirm("Make `"$gitDirUpper`" your primary git directory?"))) {
         return [EXIT_CODE]::INCOMPLETE
     }
 
-    
+    # set git environment variable
+    [Environment]::SetEnvironmentVariable("GIT_DIR", "$gitDir", "User")
 
+    # save context
+    PushCtx | Out-Null
+
+    # clone desired git repos
+    $gitRepos = 
+    "https://github.com/azydevelopment/util-powershell.git",
+    "https://github.com/azydevelopment/preferences-store.git",
+    "https://github.com/azydevelopment/core.git",
+    "https://github.com/azydevelopment/template-atmelstudio.git",
+    "https://github.com/azydevelopment/util-src-flattener.git",
+    "https://github.com/azydevelopment/template-readme.git"
+
+    Set-Location $gitDir
+    foreach ($gitRepo in $gitRepos) {
+        PushCtx | Out-Null
+        Write-Host "`n------------------"
+        $host.ui.RawUI.ForegroundColor = "Cyan"
+        Write-Host $gitRepo
+        PopCtx | Out-Null
+        git clone $gitRepo
+    }
+    
+    # copy clang-format config file into $env:GIT for use
+    Copy-Item .\preferences-store\clang\.clang-format .\
+    
+    # restore context
+    PopCtx | Out-Null
+    
+    # if we got here, all is good
     return [EXIT_CODE]::SUCCESS
 }
 
 
 ### SCRIPT START
 
+$exitCode = [EXIT_CODE]::INCOMPLETE
 try {
     # set up context stack and push down the starting context
     $gCtxStack = New-Object System.Collections.Stack
     PushCtx | Out-Null
 
     # execute main program
-    Quit(Main)
+    $exitCode = Main
+}
+catch [Exception] {
+    PrintError($_.Exception.Message)
+    $exitCode = [EXIT_CODE]::ERROR
 }
 finally {
-    Quit([EXIT_CODE]::INCOMPLETE)
+    Quit($exitCode)
 }
 
 ### SCRIPT END
