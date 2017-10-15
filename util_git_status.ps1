90c72a6a623d494e62f287dc67edf3fc82f0847f
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-10-15 00:32:55 -0700

    Made util_git_status go back to the directory you were in before it was executed

--- util_git_status.ps1
+++ util_git_status.ps1
@@ -1,11 +1,15 @@
 $gitDirs = Get-ChildItem -Directory $env:GIT
 
+$curDir = Get-Location
+
 foreach ($gitDir in $gitDirs) {
     Write-Output "----------"
     Write-Output $gitDir.Name.ToUpper()
     Write-Output `n
-    Set-Location $gitDir
+    Set-Location $gitDir.FullName
     git status
     Set-Location ..\
     Write-Output `n
 }
+
+Set-Location $curDir
