9cc9905040252e2ec343e23cc803ce686832b945
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-12-06 10:06:23 -0800

    Added lib deployment code

--- util_build_embedded_as7.ps1
+++ util_build_embedded_as7.ps1
@@ -1,56 +1,62 @@
 function ScriptMain() {
-    $solutions =
-    "embedded\embedded\embedded_as7.atsln",
-    "core\core\core_as7.atsln"
-    
+
+    $projects = $(
+        "embedded\embedded\embedded\embedded_as7.cppproj",
+        "core\core\core\core_as7.cppproj"
+    )
+
     $buildConfigs = 
     "DEBUG",
     "RELEASE"
 
     $exitCode = [EXIT_CODE]::SUCCESS
 
-    foreach ($solution in $solutions) {
+    foreach ($project in $projects) {
+        # TODO HACK: Magic slashes
+        $projectFileName = Split-Path $project -Leaf
+        $projectSubDir = Split-Path $project -Parent
+        $projectDir = "${env:GIT}\$projectSubDir"
+        $projectPath = "${env:GIT}\$project"
+
+        # TODO HACK: Magic string
+        $atmelStudioPath = "C:\Program Files (x86)\Atmel\Studio\7.0\AtmelStudio.exe"
+
         foreach ($buildConfig in $buildConfigs) {
             Write-Host "`n------------------------------"
             PushCtx | Out-Null
             $host.ui.RawUI.ForegroundColor = "Blue"
-            Write-Host "Building: ${env:GIT}\$solution"
+            Write-Host "Building: ${env:GIT}\$project"
             Write-Host "Config: $buildConfig"
             PopCtx | Out-Null
             Write-Host "------------------------------"
 
-            # TODO HACK: Magic string
-            $atmelStudioPath = "C:\Program Files (x86)\Atmel\Studio\7.0\AtmelStudio.exe"
-
             $dateTime = Get-Date -Format FileDateTime
 
             $logFileName = $(
-                ((Split-Path ${solution} -Leaf) -Replace "[.]", "_")
+                ($projectFileName -Replace "[.]", "_")
                 , "${dateTime}.txt"
             ) -Join "_"
 
-            $logFileDirectory = [io.path]::combine(
+            $logFileDir = [io.path]::combine(
                 $env:TEMP
                 , "build" # TODO HACK: Magic string
-                , $(Split-Path $solution -Parent)
-                , ${buildConfig}
+                , $projectSubDir
+                , $buildConfig
             ).toLower()
 
             # create log directory if doesn't exist
-            New-Item -ItemType Directory -Force -Path $logFileDirectory | Out-Null
+            New-Item -ItemType Directory -Force -Path $logFileDir | Out-Null
 
             $logFilePath = [io.path]::combine(
-                $logFileDirectory
+                $logFileDir
                 , $logFileName
             ).toLower()
 
-            # TODO HACK: Magic slash
-            $solutionPath = "${env:GIT}\$solution"
+            # build using Atmel Studio command line
+            Start-Process -Wait $atmelStudioPath -ArgumentList "$projectPath /build $buildConfig /out $logFilePath"
 
-            Start-Process -Wait $atmelStudioPath -ArgumentList "$solutionPath /build $buildConfig /out $logFilePath"
-
+            # print the build results
             $output = Get-Content -Path $logFilePath
-
             PushCtx | Out-Null
             Write-Host "Build: " -NoNewLine
             if ($output -match "Build FAILED") {
@@ -68,6 +74,29 @@ function ScriptMain() {
             PopCtx | Out-Null
             
             Write-Host "Log: $logFilePath"
+
+            # deploy the build results to the ${env:LIB} directory
+            $libDir = [io.path]::combine(
+                $projectDir
+                , $buildConfig
+            ).toLower()
+
+            $libDeployDir = [io.path]::combine(
+                $env:LIB
+                , $projectSubDir
+                , $buildConfig
+            ).toLower()
+
+            # create lib deploy directory if doesn't exist
+            New-Item -ItemType Directory -Force -Path $libDeployDir | Out-Null
+
+            Write-Host "Lib: " -NoNewLine
+            Write-Host $libDir
+            Write-Host $libDeployDir
+
+            # TODO HACK: Magic strings
+            Copy-Item -Path "${libDir}\*" -Include "*.a" -Destination $libDeployDir
+
             Write-Host "------------------------------"
         }
     }
